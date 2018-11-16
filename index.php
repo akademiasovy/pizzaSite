ac8a14526457ef49550d6e7763f8085d34219873
Roland <roland.onofrej@akademiasovy.sk>
2018-11-16 19:38:48 +0100

    init commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Page Title</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./css/style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <div class="container mainContainer">
+    <div class="row header">
+      <div class="col-md-8 textHeader">
+        <h2>Pizzasite</h2>
+      </div>
+      <div class="col-md-2 headerBtn">
+          <h2>Sign up</h2>
+      </div>
+      <div class="col-md-2 headerBtn">
+          <h2>Sign in</h2>
+      </div>
+    </div>
+
+    <div class="row">
+      <br><br><br>
+    </div>
+
+    <div class="row">
+      <div class="col-md-4"></div>
+      <div class="col-md-4 orderBtn">
+      </div>
+      <div class="col-md-4"></div>
+    </div>
+  </div>
+</body>
+</html>
