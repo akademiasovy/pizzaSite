0d107ccfb4fd47122f2d8282b129b3c808afc363
Mapplus <rmi7687@gmail.com>
2018-12-14 17:16:38 +0100

    update

--- js/javascript.js
+++ js/javascript.js
@@ -1,22 +1,102 @@
 function incrementAmount(element) {
-  count = parseInt(element.innerHTML);
-  element.innerHTML = count+1;
-}
-$(document).ready(function() {
-  signUp();
-});
-
-function signUp78() {
-  username = $("#signUpUsername").val();
-  console.log('test'+username);
+    count = parseInt(element.innerHTML);
+    element.innerHTML = count + 1;
 }
 
 function showSignUpForm() {
-  $("#signUpForm").css("display","inline-block");
+    $("#signUpForm").css("display", "inline-block");
 }
 
-//$('h2').click( function(){
-//  console.log('hello');
-//});
+function hideSignUpForm() {
+    $("#signUpForm").css("display", "none");
+}
 
-$("#btnSignUp").on("click",signUp);
+function signUp() {
+    if (checkSignUpForm()) {
+    //location.reload();
+    }
+}
+
+function checkSignUpForm() {
+  usernameField = $("#signUpUsername");
+  passwordField = $("#signUpPassword");
+  firstnameField = $("#signUpFirstName");
+  lastnameField = $("#signUpLastName");
+  addressField = $("#signUpAddress");
+  emailField = $("#signUpEmail");
+  phoneField = $("#signUpPhone");
+
+  correct = true;
+
+  //USERNAME CHECK
+  if (usernameField.val().length <= 0) {
+    correct = false;
+    usernameField.css("color","#DD1111");
+    usernameField.css("border","1px solid #DD1111");
+  } else {
+    usernameField.css("color","#D2D2D2");
+    usernameField.css("border","none");
+  }
+
+  //PASSWORD CHECK
+  if (passwordField.val().length < 8) {
+    correct = false;
+    passwordField.css("color","#DD1111");
+    passwordField.css("border","1px solid #DD1111");
+  } else {
+    passwordField.css("color","#D2D2D2");
+    passwordField.css("border","none");
+  }
+
+  //FIRST NAME CHECK
+  if (firstnameField.val().length <= 0) {
+    correct = false;
+    firstnameField.css("color","#DD1111");
+    firstnameField.css("border","1px solid #DD1111");
+  } else {
+    firstnameField.css("color","#D2D2D2");
+    firstnameField.css("border","none");
+  }
+
+  //LAST NAME CHECK
+  if (lastnameField.val().length <= 0) {
+    correct = false;
+    lastnameField.css("color","#DD1111");
+    lastnameField.css("border","1px solid #DD1111");
+  } else {
+    lastnameField.css("color","#D2D2D2");
+    lastnameField.css("border","none");
+  }
+
+  //ADDRESS CHECK
+  if (addressField.val().length <= 0) {
+    correct = false;
+    addressField.css("color","#DD1111");
+    addressField.css("border","1px solid #DD1111");
+  } else {
+    addressField.css("color","#D2D2D2");
+    addressField.css("border","none");
+  }
+
+  //EMAIL CHECK
+  if (emailField.val().length <= 0 || !emailField.val().match("[^@]+@(([^\.]+\..+)|localhost(:\d+)?)")) {
+    correct = false;
+    emailField.css("color","#DD1111");
+    emailField.css("border","1px solid #DD1111");
+  } else {
+    emailField.css("color","#D2D2D2");
+    emailField.css("border","none");
+  }
+
+  //PHONE NUMBER CHECK
+  if (phoneField.val().length <= 0 || !phoneField.val().match("^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$")) {
+    correct = false;
+    phoneField.css("color","#DD1111");
+    phoneField.css("border","1px solid #DD1111");
+  } else {
+    phoneField.css("color","#D2D2D2");
+    phoneField.css("border","none");
+  }
+
+  return correct;
+}
