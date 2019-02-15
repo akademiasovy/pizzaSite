8d19f7f9875553b2306989d470d95f0da880183a
akademiasovy <roland.onofrej@akademiasovy.sk>
2019-02-15 19:42:56 +0100

    Add files via upload

--- server/database.js
+++ server/database.js
@@ -35,6 +35,7 @@ var newUser = function (data) {
     con.query(sql, function (err, result) {
       if (err) throw err;
       console.log("New user created!");
+      con.end();
     });
   });
   //con.end();
@@ -44,15 +45,61 @@ var checkUser = function(username) {
   var con = connect();
 
   var sql = "SELECT * FROM login WHERE user='"+username+"'";
-  return con.query(sql, function (err, result) {
-    if (err) throw err
+
+  return Boolean(con.query(sql, function (err, result) {
+    //console.log(result.length);
+    if (err) throw err;
+    con.end();
     if (result.length > 0) {
       return true;
     } else {
       return false;
    }
-  });
+ }));
   //return check;
 }
 
-module.exports = {newUser, checkUser};
+var tokens = [];
+
+var authenticate = function(username, password) {
+  var con = connect();
+
+  const secret = 'abcdefg';
+  const passwordHash = crypto.createHmac('sha512', secret).update(data.password).digest('hex');
+  var sql = "SELECT * FROM login WHERE user='"+username+"' and passhash='"+passwordHash+"'";
+
+  return con.query(sql, function (err, result) {
+    var token = undefined;
+
+    if (result.length > 0) {
+      try {
+        tokens.forEach(function(savedToken) {
+          if (savedToken.username == username) {
+            token = savedToken.data;
+            throw new BreakException();
+          }
+        });
+      } catch(e) {
+
+      }
+
+      if (token == undefined) {
+        token = '';
+        while (token.length < 32) {
+          token = token + String.fromCharCode(97+Math.random()*25);
+        }
+        tokenObject = new Object();
+        tokenObject.username = username;
+        tokenObject.data = token;
+        //tokens[tokens.length] = JSON.stringify(tokenObject);
+        tokens=[...tokens, tokenObject];
+        console.log(tokens);
+      }
+
+    } else {
+      return false;
+    }
+ });
+};
+
+module.exports = {newUser, checkUser, authenticate};
